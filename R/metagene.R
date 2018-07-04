20d01ca7e6609fdeda0b908af9d17b4d3e31a5b6
ericfournier2 <ericfournier2@yahoo.ca>
2018-07-03 22:49:49 -0400

    Prototype simpler sampling.
    Saves time by slicing the data-table in a more efficient way (through the get_matrices function), which also allows for easier parralelization.

--- R/metagene.R
+++ R/metagene.R
@@ -1266,79 +1266,128 @@ metagene <- R6Class("metagene",
         produce_data_frame_internal = function(input_table, alpha, sample_count,
                             avoid_gaps, gaps_threshold, bam_name,
                             assay, input_design, bin_count) {
-            results <- data.table::copy(input_table)
-            
-            if(assay=='rnaseq' && !is.null(bin_count)) {
-                sample_size_columns = quote(.(region, design))
-            } else {
-                sample_size_columns = quote(.(design))
-            }
-        
-            # Set up bootstrap analysis.
-            sample_size <- input_table[bin == 1,][
-                                    ,.N, by = eval(sample_size_columns)][
-                                    , .(min(N))]
-            sample_size <- as.integer(sample_size)
-        
-            out_cols <- c("value", "qinf", "qsup")
-            bootstrap <- function(df) {
-                sampling <- matrix(df$value[sample(seq_along(df$value),
-                                        sample_size * sample_count,
-                                        replace = TRUE)],
-                                ncol = sample_size)
-                values <- colMeans(sampling)
-                res <- quantile(values, c(alpha/2, 1-(alpha/2)))
-                res <- c(mean(df$value), res)
-                names(res) <- out_cols
-                as.list(res)
-            }            
-        
-            skip_bootstrap = FALSE
-            if (assay == 'chipseq') {
-                message('produce data frame : ChIP-Seq')
-        
-                bootstrap_cols = quote(.(region, design, bin))
-                unique_col = c("region", "design", "bin", "strand")
-            } else {
-                if(avoid_gaps) {
-                    if (!is.null(bam_name)){
-                        bam_name = results$bam[1]
-                    }
-                    results = private$data_frame_avoid_gaps(results, bam_name, gaps_threshold,
-                                                            private$ph$get("flip_regions"), 
-                                                            private$ph$get("bin_count"))
-                }                
-            
-                if (is.null(bin_count)) {
-                    message('produce data frame : RNA-Seq')
-                    
-                    bootstrap_cols = quote(.(region, design, nuctot))
-                    unique_col = c("region", "design", "nuctot")
-                    
-                    if(all(rowSums(design[,-1, drop=FALSE]) == 1) &
-                        all(colSums(design[,-1, drop=FALSE]) == 1)){
-                        results$qinf <- results$value
-                        results$qsup <- results$value
-                        skip_bootstrap = TRUE
-                    }
-                } else {
-                    message('produce data frame : RNA-Seq binned')
-        
-                    bootstrap_cols = quote(.(design, bin))
-                    unique_col = c("design", "bin")
-                }
-            }
-            
-            if(!skip_bootstrap) {
-                results <- results[,c(out_cols) := bootstrap(.SD), 
-                                    by = eval(bootstrap_cols)]
-            }
-            results <- unique(results, by=unique_col)
+            #results <- data.table::copy(input_table)
+            #
+            #if(assay=='rnaseq' && !is.null(bin_count)) {
+            #    sample_size_columns = quote(.(region, design))
+            #} else {
+            #    sample_size_columns = quote(.(design))
+            #}
+            #
+            ## Set up bootstrap analysis.
+            #sample_size <- input_table[bin == 1,][
+            #                        ,.N, by = eval(sample_size_columns)][
+            #                        , .(min(N))]
+            #sample_size <- as.integer(sample_size)
+            #
+            #out_cols <- c("value", "qinf", "qsup")
+            #bootstrap <- function(df) {
+            #    sampling <- matrix(df$value[sample(seq_along(df$value),
+            #                            sample_size * sample_count,
+            #                            replace = TRUE)],
+            #                    ncol = sample_size)
+            #    values <- colMeans(sampling)
+            #    res <- quantile(values, c(alpha/2, 1-(alpha/2)))
+            #    res <- c(mean(df$value), res)
+            #    names(res) <- out_cols
+            #    as.list(res)
+            #}            
+            #
+            #skip_bootstrap = FALSE
+            #if (assay == 'chipseq') {
+            #    message('produce data frame : ChIP-Seq')
+            #
+            #    bootstrap_cols = quote(.(region, design, bin))
+            #    unique_col = c("region", "design", "bin", "strand")
+            #} else {
+            #    if(avoid_gaps) {
+            #        if (!is.null(bam_name)){
+            #            bam_name = results$bam[1]
+            #        }
+            #        results = private$data_frame_avoid_gaps(results, bam_name, gaps_threshold,
+            #                                                private$ph$get("flip_regions"), 
+            #                                                private$ph$get("bin_count"))
+            #    }                
+            #
+            #    if (is.null(bin_count)) {
+            #        message('produce data frame : RNA-Seq')
+            #        
+            #        bootstrap_cols = quote(.(region, design, nuctot))
+            #        unique_col = c("region", "design", "nuctot")
+            #        
+            #        if(all(rowSums(design[,-1, drop=FALSE]) == 1) &
+            #            all(colSums(design[,-1, drop=FALSE]) == 1)){
+            #            results$qinf <- results$value
+            #            results$qsup <- results$value
+            #            skip_bootstrap = TRUE
+            #        }
+            #    } else {
+            #        message('produce data frame : RNA-Seq binned')
+            #
+            #        bootstrap_cols = quote(.(design, bin))
+            #        unique_col = c("design", "bin")
+            #    }
+            #}
+            #
+            #if(!skip_bootstrap) {
+            #    results <- results[,c(out_cols) := bootstrap(.SD), 
+            #                        by = eval(bootstrap_cols)]
+            #}
+            results <- private$simpler_sampling(sample_count, bin_count, alpha)
             
             results <- as.data.frame(results)
             results$group <- as.factor(paste(results$design, results$region, sep="_"))
             
             return(results)
+        },
+        simpler_sampling = function(sample_count, bin_count, alpha) {
+            calc_bin_ci = function(x, sample_count, alpha) { 
+                sampled = matrix(sample(x, length(x)*sample_count, replace=TRUE), ncol=sample_count);
+                means = colMeans(sampled);
+                return(c(mean(x), quantile(means, c(alpha/2, 1-(alpha/2)))))
+            }
+            
+            calc_ci = function(x, sample_count, alpha) {
+                res = t(apply(x$input, 2, calc_bin_ci, sample_count=sample_count, alpha=alpha))
+                colnames(res) = c("value", "qinf", "qsup")
+                data.frame(res)
+            }
+
+            #ci = lapply(matrices, function(x) lapply(x, calc_ci, sample_count=sample_count, alpha=alpha))
+            #res = data.table::rbindlist(lapply(ci, data.table::rbindlist, idcol="design"), idcol="region")
+            
+            calc_ci2 = function(x, sample_count, alpha) {
+                res = t(apply(x$Matrix, 2, calc_bin_ci, sample_count=sample_count, alpha=alpha))
+                colnames(res) = c("value", "qinf", "qsup")
+                res = data.frame(res)
+                res$design = x$Coverage
+                res$region = x$Region
+                
+                res
+            }
+            
+            matrices = self$get_matrices()
+            matrix_list = list()
+            i=1
+            for(region in names(matrices)) {
+                for(coverage in names(matrices[[region]])) {
+                    matrix_list[[i]] = list(Region=region, Coverage=coverage, Matrix=matrices[[region]][[coverage]]$input)
+                    i = i + 1
+                }
+            }
+            
+            #ci = lapply(matrix_list, calc_ci2, sample_count=sample_count, alpha=alpha)
+            
+            ci <- private$parallel_job$launch_job(
+                        data = matrix_list,
+                        FUN = calc_ci2,
+                        sample_count = sample_count,
+                        alpha = alpha)
+            
+            res = data.table::rbindlist(ci, idcol=NULL, use.names=TRUE, fill=FALSE)
+            res$bin = 1:bin_count
+            
+            res
         },
         get_raw_coverages_internal = function(filenames = NULL) {
             if (is.null(filenames)) {
