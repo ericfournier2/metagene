cda762431bdccdf8123c870f6728989fbf611a19
Eric Fournier <eric.fournier@fsaa.ulaval.ca>
2018-07-11 14:50:17 -0400

    Remove RNA-seq specific code.

--- R/plot_metagene.R
+++ R/plot_metagene.R
@@ -16,164 +16,34 @@
 plot_metagene <- function(df, facet_by=NULL, group_by=NULL) {
     df$design <- as.factor(df$design)
 
-    if (('bin' %in% colnames(df)) & !('nuc' %in% colnames(df))) { 
-        # if chipseq, for instance
-        message('Plot : ChIP-Seq')
-        
-        expected_cols <- c("bin", "value", "qinf", "qsup", "group")
-        assert_subset<-df[,which(colnames(df) %in% expected_cols)]
-        expected_class <- c("integer", rep("numeric", 3), "factor")
-        names(expected_class) = expected_cols
-        stopifnot(all(expected_cols %in% colnames(assert_subset)))
-        actual_classes = vapply(assert_subset, class, character(1))
-        actual_classes = actual_classes[expected_cols]
-        stopifnot(all(actual_classes == expected_class))
+    # if chipseq, for instance
+    message('Plot metagene')
+    
+    expected_cols <- c("bin", "value", "qinf", "qsup", "group")
+    assert_subset<-df[,which(colnames(df) %in% expected_cols)]
+    expected_class <- c("integer", rep("numeric", 3), "factor")
+    names(expected_class) = expected_cols
+    stopifnot(all(expected_cols %in% colnames(assert_subset)))
+    actual_classes = vapply(assert_subset, class, character(1))
+    actual_classes = actual_classes[expected_cols]
+    stopifnot(all(actual_classes == expected_class))
 
-        if(is.null(group_by)) {
-            group_by="group"
-        }
-        
-        p <- ggplot(df, aes(x=bin, y=value, ymin=qinf, ymax=qsup)) +
-            geom_ribbon(aes_string(fill=group_by), alpha=0.3) +
-            geom_line(aes_string(color=group_by), size=1) +
-            theme(panel.grid.major = element_line()) +
-            theme(panel.grid.minor = element_line()) +
-            theme(panel.background = element_blank()) +
-            theme(panel.background = element_rect()) +
-            theme_bw(base_size = 20)
-            
-        if(!is.null(facet_by)) {
-            p <- p + facet_grid(facet_by)
-        }
-        
-        return(p)
-            
-    } else if (('nuc' %in% colnames(df)) & !('bin' %in% colnames(df))) { 
-        #message(paste('Please, notice that strand orientation by gene',
-        #            'must be the same for all BAM file.'))
-        # if rnaseq, for instance
-        ascending = function(nuc) {
-            nuc[1] < nuc[2]
-        }
-        
-        unique_with_rep = function(vect){
-            uniq <- list()
-            uniq[[1]] <- vect[1]
-            idx <- 2
-            for (i in 2:length(vect)-1) {
-                if (vect[i] != vect[i+1]) {
-                    uniq[[idx]] <- vect[i+1]
-                    idx <- idx + 1
-                }
-            }
-            return(unlist(uniq))
-        }
-        
-        #elbebg = exon_length_by_exon_by_gene
-        rev_if_flipped = function(elbebg, are_genes_unflipped) {
-            for (i in 1:length(elbebg)) {
-                #if region/gene flipped
-                if (!are_genes_unflipped[[i]]) {
-                    elbebg[[i]] <- rev(elbebg[[i]])
-                }
-            }
-            return(elbebg)
-        }
-        
-        #if only one region/gene in the data_frame
-        if (length(unique(df$region)) == 1) {
-            message('Plot : RNA-Seq (One gene)')
-            
-            are_genes_unflipped <- unlist(lapply(map(unique(df$region),
-                        ~ df[which(df$region == .x & df$bam == df$bam[1]),]$nuc)
-                                        , ascending))
-            if (all(are_genes_unflipped)){
-                exon_separation_bars <- cumsum(unique_with_rep(
-                        df$exonsize[1:length(which(df$bam == df$bam[1]))]))
-            } else {
-                exon_separation_bars <- cumsum(rev(unique_with_rep(
-                        df$exonsize[1:length(which(df$bam == df$bam[1]))])))
-            }
-        
-            expected_cols <- c("nuc", "value", "qinf", "qsup", "design")
-            df<-df[,which(colnames(df) %in% expected_cols)]
-            expected_class <- c("factor", "integer", rep("numeric", 3))
-            stopifnot(all(expected_cols %in% colnames(df)))
-            stopifnot(all(vapply(df, class, character(1)) == expected_class))
-            
-            ggplot(df, aes(x=nuc, y=value, ymin=qinf, ymax=qsup)) +
-                geom_ribbon(aes(fill = design), alpha=0.3) +
-                geom_line(aes(color = design), size=1) +
-                geom_vline(xintercept = exon_separation_bars, 
-                                                    linetype = "dotted") +
-                theme(panel.grid.major = element_line()) +
-                theme(panel.grid.minor = element_line()) +
-                theme(panel.background = element_blank()) +
-                theme(panel.background = element_rect()) +
-                theme_bw(base_size = 20)
-                
-        } else { #if multiple regions/genes in the data_frame
-            message('Plot : RNA-Seq (Multiple genes)')
-            
-            are_genes_unflipped <- unlist(lapply(map(unique(df$region),
-                        ~ df[which(df$region == .x & df$bam == df$bam[1]),]$nuc)
-                                        , ascending))
-            if (all(are_genes_unflipped)){
-                exon_separation_bars <- cumsum(unique_with_rep(
-                        df$exonsize[1:length(which(df$bam == df$bam[1]))]))
-                gene_separation_bars <- cumsum(unique_with_rep(
-                        df$regionsize[1:length(which(df$bam == df$bam[1]))]))
-            } else {
-                exon_length_by_exon_by_gene <- map(unique(df$region),
-                        ~unique_with_rep(df$exonsize[which(df$region == .x 
-                                                    & df$bam == df$bam[1])]))
-                exon_separation_bars <- cumsum(unlist(rev_if_flipped(
-                    exon_length_by_exon_by_gene,are_genes_unflipped)))
-                gene_separation_bars <- cumsum(unlist(rev_if_flipped(
-                                unique_with_rep(
-                                df$regionsize[1:length(which(
-                                                    df$bam == df$bam[1]))]), 
-                                are_genes_unflipped)))
-            }
-            
-            expected_cols <- c("nuctot", "value", "qinf", "qsup", "design")
-            df<-df[,which(colnames(df) %in% expected_cols)]
-            expected_class <- c("factor", "integer", rep("numeric", 3))
-            stopifnot(all(expected_cols %in% colnames(df)))
-            stopifnot(all(vapply(df, class, character(1)) == expected_class))
-        
-            #adjustment of nuctot in case of subset of original data frame
-            df$nuctot <- df$nuctot - min(df$nuctot) +1
-            
-            ggplot(df, aes(x=nuctot, y=value, ymin=qinf, ymax=qsup)) +
-                geom_ribbon(aes(fill = design), alpha = 0.3) +
-                geom_line(aes(color = design), size = 1) +
-                geom_vline(xintercept = exon_separation_bars, 
-                                                    linetype = "dotted") +
-                geom_vline(xintercept = gene_separation_bars, 
-                                                    linetype = "solid") +
-                theme(panel.grid.major = element_line()) +
-                theme(panel.grid.minor = element_line()) +
-                theme(panel.background = element_blank()) +
-                theme(panel.background = element_rect()) +
-                theme_bw(base_size = 20)
-        }
-    } else if (('bin' %in% colnames(df)) & ('nuc' %in% colnames(df))) { 
-        message('Plot : RNA-Seq binned')
-        
-        expected_cols <- c("bin", "value", "qinf", "qsup", "design")
-        df<-df[,which(colnames(df) %in% expected_cols)]
-        expected_class <- c("factor", "integer", rep("numeric", 3))
-        stopifnot(all(expected_cols %in% colnames(df)))
-        stopifnot(all(vapply(df, class, character(1)) == expected_class))
-
-        ggplot(df, aes(x=bin, y=value, ymin=qinf, ymax=qsup)) +
-            geom_ribbon(aes(fill=design), alpha=0.3) +
-            geom_line(aes(color=design), size=1) +
-            theme(panel.grid.major = element_line()) +
-            theme(panel.grid.minor = element_line()) +
-            theme(panel.background = element_blank()) +
-            theme(panel.background = element_rect()) +
-            theme_bw(base_size = 20)
+    if(is.null(group_by)) {
+        group_by="group"
     }
+    
+    p <- ggplot(df, aes(x=bin, y=value, ymin=qinf, ymax=qsup)) +
+        geom_ribbon(aes_string(fill=group_by), alpha=0.3) +
+        geom_line(aes_string(color=group_by), size=1) +
+        theme(panel.grid.major = element_line()) +
+        theme(panel.grid.minor = element_line()) +
+        theme(panel.background = element_blank()) +
+        theme(panel.background = element_rect()) +
+        theme_bw(base_size = 20)
+        
+    if(!is.null(facet_by)) {
+        p <- p + facet_grid(facet_by)
+    }
+    
+    return(p)
 }
