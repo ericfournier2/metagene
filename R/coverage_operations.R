1a7c71186fa8cf863a6d88fa9d376d8ad22f888d
Eric Fournier <eric.fournier@fsaa.ulaval.ca>
2018-10-04 09:47:48 -0400

    Fix bug when the number of regions is 1.

--- R/coverage_operations.R
+++ R/coverage_operations.R
@@ -30,7 +30,7 @@ bin_contiguous_regions <- function(coverage, regions, bin_count) {
   m <-  matrix(vector_means, ncol=bin_count, byrow=TRUE)
 
   # Reorder matrix to preserve region order.
-  m <- m[GenomicRanges::findOverlaps(regions, unlist(grl), select="first"),]
+  m <- m[GenomicRanges::findOverlaps(regions, unlist(grl), select="first"),, drop=FALSE]
   
   # Reverse rows on "-" strand
   mr <- m[,bin_count:1, drop=FALSE]
